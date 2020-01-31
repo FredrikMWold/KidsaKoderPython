a63d7d575b7f1afcc4359054240d5eea02dcadb1
FredrikMWold <fredrikmikalwold@gmail.com>
2020-01-31 20:12:45 +0100

    La til en del oppgave i Oppgave 4

--- Oppgave 4.py
+++ Oppgave 4.py
@@ -1,4 +1,5 @@
 """
-1. Lag en FOR-loop som skriver ut alle tallene mellom 0 og 10
+1. Lag en FOR-loop som skriver ut alle tallene mellom 0 og 100
 2. Foandre FOR-loopen slik at den kun skriver ut tall som er delelig på 2
+3. Lag en FOR-loop som leger sammen alle tallene fra 1-100
 """
