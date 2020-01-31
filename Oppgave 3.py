6406a966346ec11eb489575be76014cdacb368e1
FredrikMWold <fredrikmikalwold@gmail.com>
2020-01-31 20:09:44 +0100

    Laget oppgave 4

--- Oppgave 3.py
+++ Oppgave 3.py
@@ -1,6 +1,6 @@
 """
 1. Lag en IF-statement som sammenligner to tall.
 2. Lag en IF-statement som sammneligner to strings.
-3. Lag et program som tar to inputs, et brukernavn og et passord. Sjekk der etter om brukernavn og passord er riktig, og skriv ut en velkomst melding hvis det stemmer.
+3. Lag et program som tar to inputs, et brukernavn og et passord. Sjekk deretter om brukernavn og passord er riktig, og skriv ut en velkomst melding hvis det stemmer.
    Skriv ut en varsel melding hvis det er feil.
 """
---
+++ Oppgave 4.py
@@ -0,0 +1,4 @@
+"""
+1. Lag en FOR-loop som skriver ut alle tallene mellom 0 og 10
+2. Foandre FOR-loopen slik at den kun skriver ut tall som er delelig på 2
+"""
