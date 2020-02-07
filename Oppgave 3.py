62c061c6719af499b306b51921c0d977146d72ce
Fredrik Mikal Wold <fredrik.wold@altibox.no>
2020-02-07 08:42:09 +0100

    Laget løsnings forsalg til oppgave 3 og 4

--- Oppgave 3.py
+++ Oppgave 3.py
@@ -4,3 +4,33 @@
 3. Lag et program som tar to inputs, et brukernavn og et passord. Sjekk deretter om brukernavn og passord er riktig, og skriv ut en velkomst melding hvis det stemmer.
    Skriv ut en varsel melding hvis det er feil.
 """
+
+# Del oppgave 1
+
+tall1 = 6
+tall2 = 6
+
+if tall1 == tall2:
+    print("Rkitig")
+else:
+    print("Ikke riktig")
+
+# Del oppgave 2
+
+tekst1 = "Hey"
+tekst2 = "hey"
+
+if tekst1 == tekst2:
+    print("riktig")
+else:
+    print("Hey er ikke det samme som hey")
+
+# Del oppgave 3
+
+brukernavn = input("Skriv inn ditt brukernavn: ")
+passord = input("Skriv inn ditt passord")
+
+if brukernavn == "Fredrik" and passord == "Hemmelig":
+    print("Velkomen")
+else:
+    print("Feil brukernavn eller passord")
--- Oppgave 4.py
+++ Oppgave 4.py
@@ -3,3 +3,17 @@
 2. Foandre FOR-loopen slik at den kun skriver ut tall som er delig på 2
 3. Lag en FOR-loop som leger sammen alle tallene fra 1-100
 """
+# Del oppgave 1
+for value in range(100):
+    print(value)
+
+# Del oppgave 2
+for value in range(100):
+    if (value % 2) == 0:
+        print(value)
+
+# Del oppgave 3
+sum = 0
+for value in range(100):
+    sum = sum + value
+print(sum)
