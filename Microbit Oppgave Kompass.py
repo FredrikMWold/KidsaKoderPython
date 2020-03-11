a765174f6ef83b73705497b020414f99f9e5c874
Fredrik Mikal Wold <fredrik.wold@altibox.no>
2020-03-11 12:15:21 +0100

    update compass assignement

--- Microbit Oppgave Kompass.py
+++ Microbit Oppgave Kompass.py
@@ -1,5 +1,5 @@
 """
-Lag et program som visser hvilken vei Nord er.
+Lag et program som forteller deg om microbiten pekker mot Nord.
 Får å gjøre dette bør du sjekke denne linken: https://microbit-micropython.readthedocs.io/en/latest/compass.html
 Her står det hva microbiten sine kompass funksjoner gjør.
 """
