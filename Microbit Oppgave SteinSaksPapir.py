e4cc07386aefd587cdaafd8235a00b56445d95bb
Fredrik Mikal Wold <fredrik.wold@altibox.no>
2020-03-11 07:20:38 +0100

    la til 3 microbit oppgaver

---
+++ Microbit Oppgave SteinSaksPapir.py
@@ -0,0 +1,5 @@
+"""
+Lag et program som du kan spille stein-saks-papir mot.
+Programmet skal vente til du rister på microbiten.
+Det skal deretter velge stein, saks eller papir, og visse det på skjermen.
+"""
