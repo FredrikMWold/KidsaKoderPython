f99259534bebe6cf9e00be54c6293cea0806a0c9
FredrikMWold <fredrikmikalwold@gmail.com>
2020-09-12 13:08:41 +0200

    Update Oppgave 1.py

--- Oppgave 1.py
+++ Oppgave 1.py
@@ -1,6 +1,6 @@
 """
 1. Lag et program som printer ut noen variabler.
-2. Lag et program som legger sammen to variabler og print ut et svaret.
+2. Lag et program som legger sammen to variabler og printer ut et svaret.
 """
 
 tall1 = 3
