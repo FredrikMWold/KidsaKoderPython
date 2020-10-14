1e871a42d2b99e024a6c05870d7a34b1211d36fd
FredrikMWold <fredrikmikalwold@gmail.com>
2020-10-14 06:53:27 +0200

    Update Oppgave 6.py

--- Oppgave 6.py
+++ Oppgave 6.py
@@ -4,15 +4,3 @@ Lag et spill hvor programmet ditt velger et tilfeldig tall. Spilleren skal prøv
 Hvis spilleren gjetter feil skal programmet fortelle spillerne om de gjettet for høyt eller for lavt.
 Spilleren må da gjette på ny helt til de finner riktig tall.
 """
-
-hemmligTall = random.randint(0,100)
-
-while True:
-    gjett = int(input("Skriv inn et tall: "))
-    if gjett == hemmligTall:
-        print("Du gjettet rikrig")
-        break
-    elif gjett < hemmligTall:
-        print("Tallet er høyere")
-    elif gjett > hemmligTall:
-        print("Tallet er lavere")
