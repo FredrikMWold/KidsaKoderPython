9c6737a773e48b2d4f6003b3720d8dc2f84af952
FredrikMWold <fredrikmikalwold@gmail.com>
2020-10-14 06:52:03 +0200

    Update Oppgave 2.py

--- Oppgave 2.py
+++ Oppgave 2.py
@@ -3,14 +3,3 @@
 2. Lag en liten kalkulator som tar 2 tall som input, legger dem sammen og skriver ut svaret.
 3. Lag et program som tar navnet og alderen din som input og skriver ut en hilsen.
 """
-
-var = input("Skriv inn noe: ")
-print(var)
-
-tall1 = int(input("Skriv in et tall: "))
-tall2 = int(input("Skriv in et tall: "))
-print(tall1 + tall2)
-
-navn = input("Hva er navnet ditt: ")
-alder = input("Hvor gammel er du: ")
-print("Jeg heter " + navn + " og er " + alder + " år gammel.")
