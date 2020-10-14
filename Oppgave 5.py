493a226ccdc4fc7d03590f2c769273f902c3856d
FredrikMWold <fredrikmikalwold@gmail.com>
2020-10-14 06:52:56 +0200

    Update Oppgave 5.py

--- Oppgave 5.py
+++ Oppgave 5.py
@@ -2,8 +2,3 @@
 Lag en liste med minst 6 forskjellige tall.
 Få progammet ditt til å gå gjennom listen og pluss på 1 til alle tallene.
 """
-
-listMedTall = [23,654,324,645,23,64]
-
-for value in listMedTall:
-    print(value + 1)
